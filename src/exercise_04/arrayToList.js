7edfa586356ca60f0ada0c056eb709960c8815d4
lemon-piepie <purplechilde@163.com>
2020-07-24 21:19:07 +0800

    exercise five finished.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,13 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (!array.length) {
+    throw new Error('Creating list from undefined array');
+  }else{
+    var arr_list = new Map();
+    for(var i = 0; i < array.length; i++){
+      arr_list.add(array[i]);
+    }
+    return arr_list;
+  }
 }
--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,20 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  // 判断参数是否是数组,检测数组元素是否是整数或者数组
+  var result = [];
+  if (!Array.isArray(array)) {
+    if (array % 1 == 0) {
+      result.push(array);
+    } else {
+      throw new Error('Flatten undefined or null array');
+    }
+  }
+  else {
+    // 是数组
+    for (var i = 0; i < array.length; i++) {
+      result = result.concat(flattenArray(array[i]));
+    }
+  }
+  return result;
 }
