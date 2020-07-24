6febb28c470cbf8034f15c9244d4d878b2eb5721
lemon-piepie <purplechilde@163.com>
2020-07-24 18:45:11 +0800

    exercise two finished.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,15 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (prediction != null) {
+    let sum = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (prediction(string.charAt(i))) {
+        sum++;
+      }
+      continue;
+    }
+    return sum;
+  }
+  return string.length;
 }
