b825b4ef2d96d26ab4322afd7020b756b9663c85
lemon-piepie <purplechilde@163.com>
2020-07-28 16:39:20 +0800

    exercise seven need to learn about promises.

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,3 +1,5 @@
+import Stopwatch from '../util/stopwatch';
+
 export default function waitForAll(...promises) {
   // This function returns a promise which will be triggered when all the `promises` are completed.
   //
@@ -8,21 +10,12 @@ export default function waitForAll(...promises) {
   //
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
-  Promise.all(selectedTypes.map((type) => {
-    const body = {
-      selectedScreenshot: type,
-      dataUrl,
-    };
-    fetch(URL, {
-      method: 'POST',
-      body: JSON.stringify(body),
-      credentials: 'same-origin',
-    })
-      .then((resp) => {
-        console.log(resp.json()); //  this is done one by one as the results come in
-        console.log('next');
-      });
-  })).then((text) => {
-    console.log('done'); // this should be printed last, is printed first
-  });
+  Promise.all(promises).then(
+    res => {
+      throw new Error('It should not be a success promise');
+    },
+    err => {
+      Stopwatch.elapsedMs();
+    },
+  );
 }
